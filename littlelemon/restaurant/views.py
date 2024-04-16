928726ea006e68baf2c9316c6bbf964df3770011
Joseph Macharia <josephm2800@gmail.com>
2024-04-16 21:21:04 +0300

    Add rest_framework and rest_framework.authtoken to INSTALLED_APPS in settings.py Add API routes for tables and menu in restaurant app urls.py Create serializers for User, Menu, and Booking models in restaurant app serializers.py Create views for User, Menu, and Booking models in restaurant app views.py

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
